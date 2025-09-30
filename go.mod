ab52dd922fd9c1b9554f07cc85d7ef461fa240d3
gitlab-ci <ci@retailcrm.tech>
2025-09-30 07:48:34 +0000

    Automated release: v0.0.8

--- go.mod
+++ go.mod
@@ -7,6 +7,14 @@ require (
 	github.com/gorilla/websocket v1.5.3
 	github.com/lerenn/asyncapi-codegen v0.45.3
 	github.com/oapi-codegen/runtime v1.1.2
+	github.com/stretchr/testify v1.9.0
+	golang.org/x/time v0.5.0
 )
 
-require github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+require (
+	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- mgnode_botapi_client.go
+++ mgnode_botapi_client.go
@@ -2,6 +2,7 @@ package bot_api_client
 
 import (
 	"context"
+	"errors"
 	"net/http"
 )
 
@@ -11,4 +12,212 @@ func WithBotToken(token string) ClientOption {
 
 		return nil
 	})
+}
+
+func (r ListBotsResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r ListChannelsResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r ListChatsResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r CreateDialogResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r ListCustomersResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r ListDialogsResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r AssignDialogResponsibleResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r CloseDialogResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r DialogAddTagsResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r DialogDeleteTagsResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r UnassignDialogResponsibleResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r UploadFileResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r UploadFileByUrlResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r GetFileUrlResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r UpdateFileMetadataResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r ListMembersResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r ListMessagesResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r SendMessageResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r DeleteMessageResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r EditMessageResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r ListCommandsResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r DeleteCommandResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r CreateOrUpdateCommandResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r UpdateBotResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r ListUsersResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
+}
+
+func (r WebSocketConnectionResp) Error() error {
+	if r.JSONDefault != nil {
+		return errors.New(r.JSONDefault.Errors[0])
+	}
+
+	return nil
 }
